84ed41921fc00f64ced7dcc738f3f44f52934bff
Patrick Bellasi <patrick.bellasi@arm.com>
2015-07-07 15:33:20 +0100

    WIP: sched/{fair,tune}: track RUNNABLE tasks impact on per CPU boost value
    When per-task boosting is enabled, every time a task enters/exits a CPU
    its boost value could impact the currently selected OPP for that CPU.
    Thus, the "aggregated" boost value for that CPU potentially needs to
    be updated to match the current maximum boost value among all the tasks
    currently RUNNABLE on that CPU.
    
    This patch introduces the required support to keep track of which boost
    groups are impacting a CPU. Each time a task is enqueued/dequeued to/from
    a CPU its boost group is used to increment a per-cpu counter of RUNNABLE
    tasks on that CPU.
    Only when the number of runnable tasks for a specific boost group
    becomes 1 or 0 the corresponding boost group changes its effects on
    that CPU, specifically:
      a) boost_group::tasks == 1: this boost group starts to impact the CPU
      b) boost_group::tasks == 0: this boost group stops to impact the CPU
    In each of these two conditions the aggregation function:
      sched_cpu_update(cpu)
    could be required to run in order to identify the new maximum boost
    value required for the CPU.
    
    The proposed patch minimizes the number of times the aggregation
    function is executed while still providing the required support to
    always boost a CPU to the maximum boost value required by all its
    currently RUNNABLE tasks.
    
    BUG=chrome-os-partner:44828
    TEST=Boot kernel on Oak.
    
    Change-Id: I345e36f349381a88f0caa14abb12f9f6b06b822a
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/tune.h
+++ kernel/sched/tune.h
@@ -1,6 +1,7 @@
 
 #ifdef CONFIG_SCHED_TUNE
 
+<<<<<<< HEAD
 #include <linux/reciprocal_div.h>
 
 /*
@@ -20,22 +21,31 @@ int schedtune_task_boost(struct task_struct *tsk);
 int schedtune_prefer_idle(struct task_struct *tsk);
 
 void schedtune_exit_task(struct task_struct *tsk);
+=======
+#ifdef CONFIG_CGROUP_SCHEDTUNE
+
+int schedtune_cpu_boost(int cpu);
+>>>>>>> fbf0d16... WIP: sched/{fair,tune}: track RUNNABLE tasks impact on per CPU boost value
 
 void schedtune_enqueue_task(struct task_struct *p, int cpu);
 void schedtune_dequeue_task(struct task_struct *p, int cpu);
 
 #else /* CONFIG_CGROUP_SCHEDTUNE */
 
+<<<<<<< HEAD
 #define schedtune_cpu_boost(cpu)  get_sysctl_sched_cfs_boost()
 #define schedtune_task_boost(tsk) get_sysctl_sched_cfs_boost()
 
 #define schedtune_exit_task(task) do { } while (0)
 
+=======
+>>>>>>> fbf0d16... WIP: sched/{fair,tune}: track RUNNABLE tasks impact on per CPU boost value
 #define schedtune_enqueue_task(task, cpu) do { } while (0)
 #define schedtune_dequeue_task(task, cpu) do { } while (0)
 
 #endif /* CONFIG_CGROUP_SCHEDTUNE */
 
+<<<<<<< HEAD
 int schedtune_accept_deltas(int nrg_delta, int cap_delta,
 			    struct task_struct *task);
 
@@ -51,4 +61,11 @@ int schedtune_accept_deltas(int nrg_delta, int cap_delta,
 
 #define schedtune_accept_deltas(nrg_delta, cap_delta, task) nrg_delta
 
+=======
+#else /* CONFIG_SCHED_TUNE */
+
+#define schedtune_enqueue_task(task, cpu) do { } while (0)
+#define schedtune_dequeue_task(task, cpu) do { } while (0)
+
+>>>>>>> fbf0d16... WIP: sched/{fair,tune}: track RUNNABLE tasks impact on per CPU boost value
 #endif /* CONFIG_SCHED_TUNE */
