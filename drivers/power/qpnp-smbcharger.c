ca49d872ad950760ea4fd4aafbcff959911707c3
gabr0 <gabronogue@gmail.com>
2018-04-14 03:20:20 +0200

    Increase maximum charge input to 2A

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -499,11 +499,13 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
+
 /*
  * 1300-[smb/685mA, pmic/600mA, real usb_in/1.285A]
  * 1200-[smb/500mA, pmic/500mA, real usb_in/1A]
  */
-static int smbchg_default_hvdcp_icl_ma = 2800;
+static int smbchg_default_hvdcp_icl_ma = 2000;
+
 module_param_named(
 	default_hvdcp_icl_ma, smbchg_default_hvdcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
@@ -515,7 +517,7 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
-static int smbchg_default_dcp_icl_ma = 1500;
+static int smbchg_default_dcp_icl_ma = 2000;
 module_param_named(
 	default_dcp_icl_ma, smbchg_default_dcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
